51cea0b32e9438f6dfb89af88bc5bb6f0d33d8b1
Alexandru Safta <alexsafta98@gmail.com>
2022-06-21 16:31:57 +0300

    fix: add the contact list inside the App.js component

--- frontend-phone-book/src/components/App.js
+++ frontend-phone-book/src/components/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
+import ContactList from './ContactList';
+
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ContactList />
       </header>
     </div>
   );
--- frontend-phone-book/src/components/ContactList.js
+++ frontend-phone-book/src/components/ContactList.js
@@ -14,7 +14,6 @@ const ContactList = () => {
     })
       .then((response)=>{
         const data = response.data;
-        console.log('contacts', data);
         setContacts(data);
         setLoading(false);
       })
